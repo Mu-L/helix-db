1c467869c20ec35e5ba0eb613ea9f3f600dfd2bf
xav-db <xav@helix-db.com>
2025-09-30 11:12:36 +0100

    adding additional ci

--- helix-db/src/helix_gateway/builtin/all_nodes_and_edges.rs
+++ helix-db/src/helix_gateway/builtin/all_nodes_and_edges.rs
@@ -145,7 +145,7 @@ fn get_all_nodes_edges_json(
         });
 
         if let Some(prop) = &node_label {
-            let node = Node::decode_node(&value, id)?;
+            let node = Node::decode_node(value, id)?;
             json_node["label"] = json!(node.label());
             if let Some(props) = node.properties {
                 if let Some(prop_value) = props.get(prop) {
@@ -162,7 +162,7 @@ fn get_all_nodes_edges_json(
     let edge_iter = db.edges_db.iter(txn)?;
     for result in edge_iter {
         let (id, value) = result?;
-        let edge = Edge::decode_edge(&value, id)?;
+        let edge = Edge::decode_edge(value, id)?;
         let id_str = ID::from(id).stringify();
 
         edges.push(json!({
