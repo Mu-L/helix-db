10ccdc1654df6dbf7980d89079d54854842510f5
xav-db <xav@helix-db.com>
2025-11-09 11:34:18 -0800

    fixing query tests

--- hql-tests/tests/date_comparisons/queries.hx
+++ hql-tests/tests/date_comparisons/queries.hx
@@ -6,12 +6,3 @@ QUERY InsertVector (vector: [F64], content: String, created_at: Date) =>
     document <- AddV<Document>(vector, { content: content, created_at: created_at })
     doc <- document::{content, created_at}
     RETURN document
-
-V::CogneeVector {
-    collection_name: String,
-    data_point_id: String,
-    payload: String, // json.dumps(DataPoint) eg. (id as string, created_at, updated_at, ontology_valid, version, topological_rank, type)
-    content: String,
-    created_at: Date DEFAULT NOW,
-    updated_at: Date DEFAULT NOW,
-}
